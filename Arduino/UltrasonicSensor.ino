eb034b5ecb968a7ba74143068d1b607e6f858e0e
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2022-09-30 23:08:43 +0800

    Update UltrasonicSensor.ino

--- Arduino/UltrasonicSensor.ino
+++ Arduino/UltrasonicSensor.ino
@@ -3,16 +3,15 @@ int echoPin = 6;    // Echo
 long duration, cm, inches;
  
 void setup() {
-  //Serial Port begin
+
   Serial.begin (9600);
-  //Define inputs and outputs
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
  
 void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
