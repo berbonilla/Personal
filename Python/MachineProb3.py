270e9b2b8769c41c3f8bdffdd2ced921b92f7554
Iber Joseph Bonilla <111488477+berbonilla@users.noreply.github.com>
2022-09-30 21:16:45 +0800

    Create MachineProb3.py

---
+++ Python/MachineProb3.py
@@ -0,0 +1,15 @@
+grade=int(input("Enter score:"))
+if grade==5:
+    print("Grade: A")
+elif grade==4:
+    print("Grade: B")
+elif grade==3:
+    print("Grade: C")
+elif grade==2:
+    print("Grade: D")
+elif grade==1:
+    print("Grade: E")
+elif grade==0:
+    print("Grade: F")
+else:
+    print("Invalid Input")
